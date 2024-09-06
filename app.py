25b26ab33ae1455724751daa18def38c766d1b5f
PritamPandile <93399826+PritamPandile@users.noreply.github.com>
2024-09-06 11:00:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello_world():     
-    return 'Hello World' # main driver function 
-if __name__ == '__main__':     # run() method of Flask class runs the application      # on the local development server.     
-    app.run(host="0.0.0.0")
+from flask import Flask 
+app = Flask(__name__) 
+@app.route('/') 
+def hello_world():
+    return 'Hello World' 
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000)
